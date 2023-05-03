6aef6550ab412b6b2376e39e102015f738ea12dd
Akhila135 <128712546+Akhilar135@users.noreply.github.com>
2023-05-04 02:22:31 +0530

    Update BusTicketBooking.java

--- Bus Tickect Booking/BusTicketBooking.java
+++ Bus Tickect Booking/BusTicketBooking.java
@@ -1,34 +1,32 @@
-
 import java.util.Date;
 import java.util.Scanner;
 
 
-interface Bus {
+interface BusTicket {
     public void getBus();
+    public void userSource();
+    public void userDestination();
+    public void dateOfJourney();
 }
-interface TravelDetails{
-    public void getUserSource();
-    public void getUserDestination();
-    public void getDateOfJourney();
    
-}
-abstract class UserDetails implements Bus,TravelDetails{
-    String name,source,destination,bus1;
+abstract class UserDetails implements BusTicket{
+    String name,source,destination,busChoice;
     int age,busNum;
     Date date;
 
-    abstract public void getUserDetails();
-    abstract public void getBusDetails();
+    abstract public void userDetails();
+    abstract public void busDetails();
 
    
 }
 
-class Journey extends UserDetails implements TravelDetails,Bus{
+class Journey extends UserDetails {
     String[] busArray = {"Bus1", "Bus2", "Bus3"};
-    int [] priceArray = {1000, 1500, 2000};
+
+    int[] priceArray = {1000, 1500, 2000};
 
 @Override
-public void getUserDetails() {
+public void userDetails() {
     Scanner sc = new Scanner(System.in);
     System.out.println("------------------------");
     System.out.println("Enter Your Name:");
@@ -38,24 +36,24 @@ public void getUserDetails() {
     }
 
     @Override
-    public void getUserSource() {
+    public void userSource() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("-----------------------");
+       
         System.out.println("Enter the Source of Travel:");
         source = sc.nextLine();
     }
 
     @Override
-    public void getUserDestination() {
+    public void userDestination() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("-----------------------");
-        System.out.println("Enter destination of Travel:");
+        
+        System.out.println("Enter Destination of Travel:");
         destination = sc.nextLine();
     }
     @Override
-    public void getDateOfJourney() {
+    public void dateOfJourney() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("-----------------------");
+        
         System.out.println("Enter the Date of Journey:");
         try{
             date = new Date(sc.next());
@@ -69,56 +67,56 @@ public void getUserDetails() {
 
      @Override
     public void getBus() {
-        System.out.println("-----------------------");
+       
         System.out.println("Buses: ");
         for (int i = 0; i <3; i++){
-            System.out.println((i+1) + "."+ busArray[i] + ": Rs." + priceArray[i]);
+            System.out.println((i+1) + "-> Bus: "+ busArray[i] + "-> Tickect Price: ₹" + priceArray[i]);
         }
     }
 
     @Override
-    public void getBusDetails() {
+    public void busDetails() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("-----------------------");
+       
         System.out.println("Enter Your Bus Choice");
         busNum = sc.nextInt();
 
-        // String bus1;
+       
 
-        switch(busNum){
-            case 1:
-            bus1 = busArray[0];
-            break;
-
-            case 2:
-            bus1 = busArray[1];
-            break;
-
-            case 3:
-            bus1 = busArray[2];
-            break;
-            
-            default:
-            System.out.println("Enter correct details");
-            break;
+        if (busNum == 0){
+            busChoice = busArray[0];
         }
-        
-    }
+        else if(busNum == 1){
+            busChoice = busArray[1];
+            }
 
-    public void showFullDetails(){
-        System.out.println("-----------------------");
-        System.out.println("Booking Completed");
-        System.out.println("Source:" + source + " " + "Destination:" + destination);
-        System.out.println("Date:" + date);
-        System.out.println("Bus Selected:" + bus1);
-        System.out.println("Name:" + name +  " " + "Age:" + age);
+         else if(busNum == 2){
+                busChoice = busArray[2];
+                }
+         else{
+            System.out.println("Sorry... Bus could not find.");
+            System.out.println("Please provide correct details");
+         }   
+          
+            
+        }
 
-    }
 
-    @Override
-    public void getBusDetails() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getBusDetails'");
+    public void displayBookingSummary(){
+
+        System.out.println("Successfully booked the ticket");
+        System.out.println("***Enjoy your journey***");
+        System.out.println("__________________________");
+
+        System.out.println("Booking Summary");
+
+        System.out.println("Source => " + source);
+        System.out.println("Destination => " + destination);
+        System.out.println("Date: " + date);
+        System.out.println("Bus Name: " + busChoice);
+        System.out.println("---User Details---");
+        System.out.println("Name: " + name +" || " +"Age: " + age);
+
     }
 
 }
@@ -130,17 +128,21 @@ public class BusTicketBooking {
         Scanner sc = new Scanner(System.in);
         Journey j1 = new Journey();
         System.out.println("...Journey Ticket Booking....");
+
         while(choice == 1){
-        j1.getUserSource();
-        j1.getUserDestination();
-        j1.getDateOfJourney();
-        j1.showBus();
+        j1.userSource();
+        j1.userDestination();
+        j1.dateOfJourney();
         j1.getBus();
-        j1.getUserDetails();
-        j1.showFullDetails();
-        System.out.println("1.Book another ticket?");
+        j1.busDetails();
+        j1.userDetails();
+        j1.displayBookingSummary();
+
+        System.out.println("1.Book more tickets");
+
         System.out.println("2.Leave the page!");
-        System.out.println("Enter Your choice:");
+
+        System.out.println("Select option");
         choice = sc.nextInt();
         }
     }
