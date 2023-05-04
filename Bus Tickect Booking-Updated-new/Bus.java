dac43da86db17a30c583f8432cecc7a1142d3bcf
Akhila135 <128712546+Akhilar135@users.noreply.github.com>
2023-05-05 01:23:37 +0530

    Add files via upload

---
+++ Bus Tickect Booking-Updated-new/Bus.java
@@ -0,0 +1,17 @@
+public class Bus {
+    
+    String busName;
+    int price;
+    int num;
+    public Bus(String busName, int price,int num) {
+
+        this.busName=busName;
+        this.price=price;
+        this.num=num;
+    }
+    @Override
+    public String toString(){
+return (this.num+"->Bus: "+this.busName+"  Price: "+this.price+"\n");
+    }
+
+}
